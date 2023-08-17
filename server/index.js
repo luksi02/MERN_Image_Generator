43df9f2f5d0d958973f5c45a9a7287fb84bd8dbb
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-08-17 23:52:05 +0200

    10.0?

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
@@ -11,9 +13,16 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E');
 })
+
+// app.get('/another/', async (req, res) => {
+//     res.send('Hello from DALL-E - another');
+// })
 
 const startServer = async () => {
 
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,99 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+    // API KEY below:
+    
+    // apiKey: process.env.DALLE_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.status(200).json( {message:'Hello from DALL-E!'} )
+    // res.send('Hello from DALL-E! This time API should work')
+})
+
+router.route('/').post( async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        console.log(prompt)
+
+        const aiResponse = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        })
+
+        console.log(aiResponse.data.data[0].url)
+        console.log(aiResponse)
+
+
+        // const image = aiResponse.data.b64_json;
+
+        // res.status(200).json({ photo: image });
+
+    }
+    catch (error) {
+
+        // console.log(error);
+
+        console.log(error.response.status)
+        console.log(error.response.data)
+
+
+        // res.status(500).send(error?.response.data.error.message)
+
+    }
+})
+
+export default router;
+
+
+
+//
+// import express from 'express';
+// import * as dotenv from 'dotenv';
+// import { Configuration, OpenAIApi } from 'openai';
+//
+// dotenv.config();
+//
+// const router = express.Router();
+//
+// const configuration = new Configuration({
+//     apiKey: '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',
+// });
+//
+// const openai = new OpenAIApi(configuration);
+//
+// router.route('/').get((req, res) => {
+//     res.status(200).json({ message: 'Hello from DALL-E!' });
+// });
+//
+// router.route('/').post(async (req, res) => {
+//     try {
+//         const { prompt } = req.body;
+//
+//         const aiResponse = await openai.createImage({
+//             prompt,
+//             n: 1,
+//             size: '1024x1024',
+//             response_format: 'b64_json',
+//         });
+//
+//         const image = aiResponse.data.data[0].b64_json;
+//         res.status(200).json({ photo: image });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+//     }
+// });
+//
+// export default router;
