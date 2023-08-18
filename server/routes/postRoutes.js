e3016c6e303cf67845049d535621a23e83284c5d
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-08-19 01:23:59 +0200

    12.0?

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -14,4 +14,14 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
+});
+
 export default router;
