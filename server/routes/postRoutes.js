8497f103cc582fd0dbfd3de16d79fcdc0025c980
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-08-18 23:48:59 +0200

    11.0?

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,10 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export default router;
