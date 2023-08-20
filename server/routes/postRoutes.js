a446c271f99ce120ce2dc347e5aabc0b6777ffa7
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-08-20 23:56:19 +0200

    13?

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -24,4 +24,10 @@ router.route('/').get(async (req, res) => {
   }
 });
 
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
 export default router;
