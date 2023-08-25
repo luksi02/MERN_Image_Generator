5e92046bd839f9e8d7c64680137ee90bb883d85b
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-08-25 17:36:30 +0200

    19

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -64,8 +64,8 @@ const Home = () => {
         setSearchTimeout(
             setTimeout(() => {
                 const searchResults = allPosts.filter((item) => item.
-                    name.toLowerCase().includes(searchText.toLowerCase
-                    ()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+                    name.toLowerCase().includes(searchText.toLowerCase()) || item.
+                    prompt.toLowerCase().includes(searchText.toLowerCase()));
 
                 setSearchedResults(searchResults);
             }, 500)
@@ -85,7 +85,14 @@ const Home = () => {
                 </p>
 
                 <div className="mt-16">
-                    <FormField />
+                    <FormField
+                        labelName="Search posts"
+                        type="text"
+                        name="text"
+                        placeholder="Search something..."
+                        value={searchText}
+                        handleChange={handleSearchChange}
+                    />
                 </div>
 
                 <div className="'mt-10">
@@ -103,7 +110,7 @@ const Home = () => {
                             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                                 {searchText ? (
                                     <RenderCards
-                                        data={[]}
+                                        data={searchedResults}
                                         // data="searchedResults"
                                         title="No search results found"
                                     />
